f1611061076304161ab196e7b17d883986fcb276
yongdori00 <yongdori98@gmail.com>
2022-03-15 22:05:24 +0900

    GaBom start

---
+++ src/main/java/com/example/bom/gabom/GaBomApplication.java
@@ -0,0 +1,13 @@
+package com.example.bom.gabom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaBomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaBomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bom/gabom/GaBomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bom.gabom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
