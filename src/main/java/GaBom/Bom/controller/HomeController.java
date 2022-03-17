80285948cb5dcccf04b1e7f09ea35c7682ec179b
rlatldhs32 <rlatldhs32@naver.com>
2022-03-17 19:56:16 +0900

    220317 Update

---
+++ src/main/java/GaBom/Bom/controller/HomeController.java
@@ -0,0 +1,39 @@
+package GaBom.Bom.controller;
+
+import GaBom.Bom.model.Board;
+import GaBom.Bom.model.User;
+import GaBom.Bom.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/list")
+    public String MakeUser(@RequestBody User user){
+        userRepository.save(user);
+        return user.getUsername();
+    }
+
+    @GetMapping("/list")
+    public List<User> UserList(){
+        List<User> users = userRepository.findAll();
+        return users;
+    }
+
+//    @DeleteMapping("/list/{user_id}")
+//    public String DeleteUser(@){
+//        userRepository.f
+//        List<User> users = userRepository.findAll();
+//        return users;
+//    }
+
+
+}
